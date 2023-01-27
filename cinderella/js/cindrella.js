81dbd9e1074557eae5920c14befc5432b2502866
Maya Lev <maya.lev.2009@gmail.com>
2023-01-27 20:35:05 +0200

    cinderella fixed button

--- cinderella/js/cindrella.js
+++ cinderella/js/cindrella.js
@@ -1,7 +1,4 @@
-function page1(){
-  document.getElementById("door").style.display = "block";
-}
-function right(){
-    alert("yes!!!")
-    document.getElementById("door").style.display = "none";
+
+function nextRoom(){
+    document.getElementById("door").style.display = "block";
   }
