e4b426615adab13bc10d05f742d3ce7cae575485
Galia <glyhdwnh556@gmail.com>
2023-01-27 20:27:32 +0200

    Adding a door

--- cinderella/js/cindrella.js
+++ cinderella/js/cindrella.js
@@ -1,3 +1,7 @@
+function page1(){
+  document.getElementById("door").style.display = "block";
+}
 function right(){
     alert("yes!!!")
+    document.getElementById("door").style.display = "none";
   }
