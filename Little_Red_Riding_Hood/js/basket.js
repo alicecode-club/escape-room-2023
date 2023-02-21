0c2eb8e23436891796f20439f4093abbb3cd8e8b
Your Name <youremail@yourdomain.com>
2023-02-21 18:13:56 +0200

    basket update

--- Little_Red_Riding_Hood/js/basket.js
+++ Little_Red_Riding_Hood/js/basket.js
@@ -3,4 +3,8 @@ let counter = 0;
 function items_counter() {
     counter = counter+1;
     console.log(counter);
+
+    if(counter == 4) {
+        window.location.replace("html/last_page.html");
+    }
 }
