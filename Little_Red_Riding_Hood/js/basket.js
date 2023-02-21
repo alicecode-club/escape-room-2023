c230e60dbfc988295f4a039a0188df06137dea68
Your Name <youremail@yourdomain.com>
2023-02-21 19:41:24 +0200

    basket

--- Little_Red_Riding_Hood/js/basket.js
+++ Little_Red_Riding_Hood/js/basket.js
@@ -4,7 +4,20 @@ function items_counter() {
     counter = counter+1;
     console.log(counter);
 
+    if(counter == 1) {
+        document.getElementById("items_counter").innerHTML = "Items counter: 1/4";
+    }
+
+    if(counter == 2) {
+        document.getElementById("items_counter").innerHTML = "Items counter: 2/4";
+    }
+
+    if(counter == 3) {
+        document.getElementById("items_counter").innerHTML = "Items counter: 3/4";
+    }
+
     if(counter == 4) {
-        window.location.replace("html/last_page.html");
+        document.getElementById("items_counter").innerHTML = "Items counter: 4/4";
+        /*window.location.replace("html/last_page.html");*/
     }
 }
