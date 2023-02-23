7acdf69a2d4cd2d9a49f3a45703c26e3ce81a183
AdiAsaf1805 <adiasaf181@gmail.com>
2023-02-23 18:59:17 +0200

    Puting css and js of the last page in new files

---
+++ Little_Red_Riding_Hood/js/last_page.js
@@ -0,0 +1,21 @@
+function grandma(){
+    alert("First Number Is 2")
+}
+function wolf(){
+    alert("Second Number Is 3")
+}
+function cape(){
+    alert("Third Number Is 8")
+}
+function book(){
+    alert("Forth Number Is 6")
+}
+function door(){
+    let code=prompt("Enter Code:")
+    if (code=="2386"){
+        window.location.assign("../../cinderella/html/cinderella.html")
+    }
+    else{
+        alert("Sorry, Try Again...")
+    }
+}
