0454d8a66e12d9a381f0689c69377c1564e17174
Maya Lev <maya.lev.2009@gmail.com>
2023-02-28 16:08:21 +0200

    cinderella room update

--- cinderella/js/cindrella.js
+++ cinderella/js/cindrella.js
@@ -2,3 +2,7 @@
 function doorAppear(){
     document.getElementById("door").style.display = "block";
   }
+
+  function nextRoom(){
+    window.location.assign("../../cinderella/html/cinderella.html")
+  }
