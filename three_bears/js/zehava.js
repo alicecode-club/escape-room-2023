0c416205b5fdd9463e301e5c41e6393e1bc3d335
nikolosh <nicolrahnaev@gmail.com>
2023-03-08 20:33:52 +0200

    added alice quetion

--- three_bears/js/zehava.js
+++ three_bears/js/zehava.js
@@ -1,3 +1,14 @@
+// ~ Start of js part 1/2 to add. Change:
+// 1. relativeLocationToAliceFolder${fairyTaleroomNumber} = your relative location to /alicecode_questions/.
+// 2. fairyTaleroomNumber = correct room number (1-6):
+//   1: Little red riding hood, 2: Cinderella, 3: Hansel and Gretel, 4: The little mermaid
+//   5: Zehava and the three bears, 6: The school for good and evil
+let fairyTaleroomNumber = 5;
+
+localStorage.setItem("roomNumber",fairyTaleroomNumber);
+localStorage.setItem(`relativeLocationToAliceFolder${fairyTaleroomNumber}`, "../../alicecode_questions/");
+// ~ End of js part 1/2 to add 
+
 // Question 1
 var answer1 = "mother father and son";
 
@@ -5,6 +16,7 @@ function checkAnswer1() {
     var userAnswer = document.getElementById("answer1").value;
     if (userAnswer === answer1) {
         localStorage.setItem("answer1", true);
+        document.getElementById("q1img").style.border=" 10px solid black;"
         alert("Correct! You answered " + userAnswer);
         window.location.href = "index.html";
     } else {
@@ -55,7 +67,18 @@ function doorAppear(){
     document.getElementById("door1").style.visibility="visible";
 }
 function nextroom(){
-    window.location.assign("../../the_school_for_good_and_evil/forest1.html")
+    // ~ Start of js part 2/2 to add: Add THIS in the function that lead to the next page
+    if(localStorage.getItem(`alice_question${fairyTaleroomNumber}_answered`) !== "true"){
+        alert("You need to answer alice's question before you can leave!");
+    }
+    else{
+        // escape the room! = your code of moving to next page
+        window.location.assign("../../the_school_for_good_and_evil/forest1.html")
+
+        //just for testing
+        localStorage.removeItem(`alice_question${fairyTaleroomNumber}_answered`);
+    }
+    // ~ End of js part 2/2 to add
 
 
 }
