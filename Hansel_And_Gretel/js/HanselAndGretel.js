63b3ab97fcb5b0e8d4991ba25e43beca62635b0b
MAYAoph <maya.ophir.2@gmail.com>
2023-03-09 19:31:19 +0200

    you escaped update

--- Hansel_And_Gretel/js/HanselAndGretel.js
+++ Hansel_And_Gretel/js/HanselAndGretel.js
@@ -1,6 +1,18 @@
 let degreesPuzzle=false;
 let cagePuzzle=false;
 
+   // ~ Start of js part 1/2 to add. Change:
+        // 1. relativeLocationToAliceFolder${fairyTaleroomNumber} = your relative location to /alicecode_questions/.
+        // 2. fairyTaleroomNumber = correct room number (1-6):
+        //   1: Little red riding hood, 2: Cinderella, 3: Hansel and Gretel, 4: The little mermaid
+        //   5: Zehava and the three bears, 6: The school for good and evil
+        let fairyTaleroomNumber = 3;
+
+        localStorage.setItem("roomNumber",fairyTaleroomNumber);
+        localStorage.setItem(`relativeLocationToAliceFolder${fairyTaleroomNumber}`, "../../alicecode_questions/");
+        // ~ End of js part 1/2 to add
+
+
 function exit() {
     code_solved= localStorage.getItem("code_solved");
     console.log(code_solved);
@@ -9,9 +21,20 @@ function exit() {
         window.location.replace("../html/colorPuzzle.html");
     }
     else {
-        alert("you escaped!");
-        localStorage.removeItem("code_solved");
-        window.location.replace("../../the_little_mermaid/one.littel.html");
+                        // ~ Start of js part 2/2 to add: Add THIS in the function that lead to the next page
+                if(localStorage.getItem(`alice_question${fairyTaleroomNumber}_answered`) !== "true"){
+                    alert("You need to answer alice's question before you can leave!");
+                }
+                else{
+                    // escape the room! = your code of moving to next page
+                    localStorage.removeItem("code_solved");
+                    alert("you escaped!");
+                    window.location.replace("../../the_little_mermaid/one.littel.html");
+
+                    //just for testing
+                    localStorage.removeItem(`alice_question${fairyTaleroomNumber}_answered`);
+                }
+                // ~ End of js part 2/2 to add
         
     }
 }
