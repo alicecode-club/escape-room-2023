7232873bdddc309520f4f05b1047f69ff1c66472
Galia <glyhdwnh556@gmail.com>
2023-03-10 19:34:52 +0200

    Small changes

--- Openning_Page.js
+++ Openning_Page.js
@@ -1,3 +1,6 @@
 function litteRedRidingHoodRoom(){
     window.location.assign("Little_Red_Riding_Hood/html/Little_Red_Riding_Hood.html");
 }
+function picture(){
+    alert("To start you need to click on the door")
+}
